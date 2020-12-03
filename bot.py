8860bc8a44d3eb716d5b6780427844041e15c68b
zasd123 <62350166+zasd123@users.noreply.github.com>
2020-12-02 21:31:06 -0500

    fixed send message feature
    fixed send message feature

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import discord
 import random
 import numpy as np
-
 from discord.ext import commands
 
 
 participants = []
+
 client = commands.Bot( command_prefix = '.')
 
 
@@ -14,25 +14,14 @@ async def on_ready():
         print('bot is ready')
 
 
-@client.event
-async def on_member_join(member):
-        print(f'{member} has joined a server.')
-
-@client.event
-async def on_member_remove(member):
-        print(f'{member} has left a server.')
-
-@client.command()
-async def ping(ctx):
-        await ctx.send('Pong!')
-
 @client.command()
 async def register(ctx):
+        responses = []
         await ctx.send("name? : ")
 
         def check(msg):
                 return msg.author == ctx.author and msg.channel == ctx.channel
-        responses = []
+
         msg = await client.wait_for("message", check=check)
         responses.append(msg.content)
         await ctx.send("email? : ")
@@ -41,12 +30,12 @@ async def register(ctx):
         await ctx.send("address? : ")
         msg = await client.wait_for("message", check=check)
         responses.append(msg.content)
+        await ctx.send("notes? : ")
+        msg = await client.wait_for("message", check=check)
+        responses.append(msg.content)
         responses.append(msg.author)
         await ctx.send("You have been registered")
-
-
-
-        participants.append(participant(responses[0],responses[1],responses[2],responses[3]))
+        participants.append(participant(responses[0],responses[1],responses[2],responses[3],responses[4]))
 
 @client.command()
 async def shufflesend(ctx):
@@ -58,12 +47,10 @@ async def shufflesend(ctx):
         if (msg.content == "shufflesend"):
                 if (len(participants) % 2 ==0):
                         random.shuffle(participants)
-
-#                       sendmail(participants)
+                        await sendmail()
                         await ctx.send("done")
                 else:
                         await ctx.send("there is an odd number of participants. try again later.")
-
 
 
 @client.command()
@@ -71,24 +58,34 @@ async def lp(ctx):
         for x in range(len(participants)):
                 await ctx.send(participants[x].__dict__)
 
+@client.command()
+async def clear(ctx):
+        participants.clear()
+        await ctx.send("cleared array")
 
-def sendmail(list):
-        for person in list:
-                print ('sent ', person.name, ' a message')
-
-def pair(list):
-        merged_list = []
-        half = np.array_split(list, (len(list)/2))
-        merged_list = tuple(zip(list, half))
-        for pairing in merged_list:
-                print(pairing)
-        return merged_list
 
 
 class participant:
-        def __init__(self, name, email, address, id):
+        def __init__(self, name, email, address, notes, id):
                 self.name = name
                 self.email = email
                 self.address = address
+                self.notes = notes
                 self.id = id
 
+async def sendmail():
+        for x in range(len(participants)):
+                if(x != len(participants)-1):
+                        user_disc = client.get_user(participants[x].id.id)
+                        user = (participants[x])
+                        next = (participants[x+1])
+                        output = ('Hello ' + user.id.name + '\n You got paried with ' + next.id.name + '\n here is their info: ' + '\n IRL Name: '+ next.name + '\n Email : ' + next.email + '\n Address : ' + next.address + '\n Notes : ' + next.notes)
+                        await user_disc.send(output)
+
+
+                else:
+                        user_disc = client.get_user(participants[x].id.id)
+                        user = (participants[x])
+                        first = (participants[0])
+                        output = ('Hello ' + user.id.name  + '\n You got paried with ' + first.id.name + '\n here is their info:  ' + '\n IRL Name: '+ first.name + '\n Email : ' + first.email + '\n Address : ' + first.address + '\n Notes : ' + first.notes)
+                        await user_disc.send(output)
